c9bd4c079e00d031fe45bdb0a61ffc301e339701
tangentstorm <michal.wallace@gmail.com>
2021-12-09 19:27:31 -0500

    demonstrate fetching data with JGetM

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 //
 // https://code.jsoftware.com/wiki/Interfaces/JFEX
 // https://github.com/jsoftware/jsource/blob/master/jsrc/jlib.h
+// https://github.com/jsoftware/jsource/blob/master/jsrc/io.c
 // https://code.jsoftware.com/wiki/Guides/DLLs/Calling_the_J_DLL
 
 #[macro_use] extern crate dlopen_derive;
@@ -34,8 +35,11 @@ pub type JI = i64;
 /// opaque pointer to J interpreter.
 #[repr(C)] #[derive(Clone,Copy)] pub struct JT(*const u8);
 
-/// j array type (unused so far)
-#[repr(C)] pub struct JA { k:JI, flag:JI, m:JI, t:JI, c:JI, n:JI, r:JI, s:JI, v:*const JI }
+// pointer to j integer
+pub type PJI = *const JI;
+
+/// j array type
+#[repr(C)] #[derive(Debug)] pub struct JA { k:JI, flag:JI, m:JI, t:JI, c:JI, n:JI, r:JI, s:PJI, v:PJI }
 
 /// code indicating the type of session. sent to jsm()
 #[repr(C)] pub struct SMTYPE(usize);
@@ -99,23 +103,38 @@ pub struct JAPI {
   #[dlopen_name="JSM"]   jsm: extern "C" fn(jt:JT, jcbs:JCBs),
   // #[dlopen_name="JSMX"]  jsmx: extern "C"
   // fn(jt:JT, wr:JWrFn, wd:JWdFn, rd:JRdFn, _x:VOIDP, sm:SMTYPE)
-  /// fetch byte representation: (3!:1) name
-  #[dlopen_name="JGetA"] geta: extern "C" fn(jt:JT, ji:JI, nm:JS)->JA,
+
+  /// fetch byte representation: (3!:1) name. len seems to be the length of the name
+  #[dlopen_name="JGetA"] geta: extern "C" fn(jt:JT, len:JI, nm:JS)->JA,
 
   /// get named noun as (type, rank, shape, data)
-  #[dlopen_name="JGetM"] getm: extern "C" fn(jt:JT, nm:JS, t:&mut JI, r:&mut JI, sh:&mut VOIDP, d:&mut VOIDP),
+  #[dlopen_name="JGetM"] getm: extern "C" fn(jt:JT, nm:JS, t:&mut JI, r:&mut JI, sh:&mut PJI, d:&mut VOIDP),
 
   /// set named noun as (type, rank, shape, data)
-  #[dlopen_name="JSetM"] setm: extern "C" fn(jt:JT, nm:JS, t:&mut JI, r:&mut JI, sh:&mut VOIDP, d:&mut VOIDP) }
+  #[dlopen_name="JSetM"] setm: extern "C" fn(jt:JT, nm:JS, t:&mut JI, r:&mut JI, sh:&mut PJI, d:&mut VOIDP) }
 
 pub fn load<'a>()->Container<JAPI> { unsafe { Container::load(JDL).unwrap() }}
 
-/// run with `cargo test --lib -- --nocapture`
+/// run with `cargo test --lib`   # add `-- --nocapture` to see println!() calls
 #[test]fn test_demo() {
+  // connect to j and run a simple command:
   let c = load();
   let jt = c.init();
-  let s = jstr!(b"i. 3 3\0");
-  c.jdo(jt, s);
-  let r = c.getr(jt);
-  assert_eq!("0 1 2\n3 4 5\n6 7 8\n", jfmt!(r));
+  c.jdo(jt, jstr!(b"m =. *: i. 2 3\0"));
+  c.jdo(jt, jstr!(b"m\0"));
+  assert_eq!("0  1  4\n9 16 25\n", jfmt!(c.getr(jt)));
+
+  // now fetch the actual data.
+  let mut t:JI=0; let mut r:JI=0; let mut sh:PJI=std::ptr::null_mut(); let mut d:VOIDP=std::ptr::null_mut();
+  c.getm(jt, jstr!(b"m\0"), &mut t, &mut r, &mut sh, &mut d);
+  const JDT_INT:JI = 4; assert_eq!(JDT_INT, t);
+  assert_eq!(2, r); // answer is a rank 2 array
+  let rows = unsafe { *sh }; assert_eq!(rows, 2, "expect 2 rows");
+  let cols = unsafe { sh = sh.add(1); *sh }; assert_eq!(cols, 3, "expect 3 cols");
+  let mut res : Vec<JI> = vec![];
+  let mut d = d as *const JI; // since t told us it's an array of J ints.
+  unsafe { for _ in 0..(rows*cols) { res.push(*d); d = d.add(1); }}
+  assert_eq!(res, [0, 1, 4, 9, 16, 25]);
+
+  // all done. kill the session:
   c.free(jt); }
